e37f29ba31a38cc23bea5e1fe8016aac5b015183
abdalla_emad <abdallaemad1.3.2.0.0.5@gmail.com>
2025-10-02 15:07:40 +0300

    Add create, get and delete categories logic

---
+++ src/constant/index.ts
@@ -0,0 +1,24 @@
+export const COLORS_OPTIONS = [
+  "#FF6B6B", // bg-[#FF6B6B]
+  "#4ECDC4", // bg-[#4ECDC4]
+  "#45B7D1", // bg-[#45B7D1]
+  "#FFA07A", // bg-[#FFA07A]
+  "#98D8C8", // bg-[#98D8C8]
+  "#FDCB6E", // bg-[#FDCB6E]
+  "#6C5CE7", // bg-[#6C5CE7]
+  "#FF85A2", // bg-[#FF85A2]
+  "#2ECC71", // bg-[#2ECC71]
+  "#E17055", // bg-[#E17055]
+];
+export const EMOJI_OPTIONS = [
+  { emoji: "💰", label: "Money (Sale)" },
+  { emoji: "👤", label: "User (Sign-up)" },
+  { emoji: "🎉", label: "Celebration" },
+  { emoji: "📅", label: "Calendar" },
+  { emoji: "🚀", label: "Launch" },
+  { emoji: "📢", label: "Announcement" },
+  { emoji: "🎓", label: "Graduation" },
+  { emoji: "🏆", label: "Achievement" },
+  { emoji: "💡", label: "Idea" },
+  { emoji: "🔔", label: "Notification" },
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseColor(color: string) {
+  const hexCode = color.replace("#", "");
+  return parseInt(hexCode, 16);
 }
